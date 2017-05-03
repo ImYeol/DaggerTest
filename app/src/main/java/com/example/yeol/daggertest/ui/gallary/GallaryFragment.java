b9ed6415a5dab2c7229ddbec0a0db69c3275abab
ImYeol <gyl115@naver.com>
2017-05-03 18:13:03 +0900

    start gallery

--- app/src/main/java/com/example/yeol/daggertest/ui/gallary/GallaryFragment.java
+++ app/src/main/java/com/example/yeol/daggertest/ui/gallary/GallaryFragment.java
@@ -4,5 +4,21 @@ package com.example.yeol.daggertest.ui.gallary;
  * Created by yeol on 17. 4. 21.
  */
 
+/**
+ * Glide.with(context)
+ .load(imageByteArray)
+ .asBitmap()
+ .placeholder(R.drawable.ic_broken)
+ .into(rowImageView);
+ */
+
+/**
+ *   https://github.com/martin90s/ImagePicker
+ *   https://github.com/rezaamostafavi/Android-GalleryView
+ *   https://github.com/lauw/Android-SnappingRecyclerView
+ *   https://github.com/Suleiman19/Gallery
+ *   https://github.com/suhanlee/recyclerview-image-gallery
+ */
+
 public class GallaryFragment {
 }
